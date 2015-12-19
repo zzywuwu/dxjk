2cf99df76ad5e51153f20bd1b169a23d0e05d3de
zzywuwu <zzywuwu@126.com>
2015-12-19 14:29:06 +0800

    修改数据库备份每天

--- custom_service_system/db/backupdb.lua
+++ custom_service_system/db/backupdb.lua
@@ -1,5 +1,5 @@
-package.path = package.path..";/usr/lib64/lua/5.1/?.lua;/opt/nginx/lua_scripts/?.lua"
-package.cpath = package.cpath..";/usr/lib64/lua/5.1/?.so"
+package.path = package.path..";/usr/lib64/lua/5.1/?.lua;/opt/nginx/lua_scripts/?.lua;/usr/local/share/lua/5.1/?.lua"
+package.cpath = package.cpath..";/usr/lib64/lua/5.1/?.so;/usr/local/lib/lua/5.1/?.so;/usr/local/share/lua/5.1/?.so"
 require("socket")
 
 local full_path = "/var/logs/fullbackup/"
@@ -19,7 +19,7 @@ local function BackupFullDB(time)
     local min = date.min 
     --1 for sunday
     local wday = date.wday
-    if wday == 1 then
+    -- if wday == 1 then
     --3:00~3:10
         if hour*60+min - 180 >= 0 and hour*60+min -190 < 0 then
             --do backup full
@@ -31,7 +31,7 @@ local function BackupFullDB(time)
         --print(command)
             os.execute(command)
         end
-    end
+    -- end
 end
 
 local function BackupIncDB(time)
@@ -43,14 +43,15 @@ local function BackupIncDB(time)
         --if hour*60+min - 180 >= 0 and hour*60+min -190 < 0 then
         if hour*60+min - 300 >= 0 and hour*60+min - 310 < 0 then
             --do backup increment
-        local day_3_ago = os.date("*t", time-60*60*24*3)
-        local day_3_ago_str = day_3_ago.year..day_3_ago.month..day_3_ago.day..day_3_ago.hour..day_3_ago.min..day_3_ago.sec
-        local command = "mysqladmin -uroot flush-logs && mysql -uroot -pserver -e \"purge master logs before "..day_3_ago_str.."\""
-        os.execute(command)
-        command = "cp /var/logs/mysql/dtjx-bin.* "..increment_path
-        os.execute(command)
-        command = "echo "..date.year.."-"..date.month.."-"..date.day.."-"..date.hour.."-"..date.min.."-"..date.sec.." Done Increament Backup >> "..increment_path.."incdb.log"
-        os.execute(command)
+            local day_3_ago = os.date("*t", time-60*60*24*3)
+            local day_3_ago_str = day_3_ago.year..day_3_ago.month..day_3_ago.day..day_3_ago.hour..day_3_ago.min..day_3_ago.sec
+            local command = "mysqladmin -uroot flush-logs && mysql -uroot -pserver -e \"purge master logs before "..day_3_ago_str.."\""
+            os.execute(command)
+            command = "cp /var/logs/mysql/dtjx-bin.* "..increment_path
+            os.execute(command)
+            command = "echo "..date.year.."-"..date.month.."-"..date.day.."-"..date.hour.."-"..date.min.."-"..date.sec.." Done Increament Backup >> "..increment_path.."incdb.log"
+            os.execute(command)
+            DeleteData()
         end
     --end
 end
@@ -65,7 +66,6 @@ end
 CreateBakeupDir()
 while true do
     local time = GetTimeNow()
-    DeleteData()
     BackupFullDB(time)
     BackupIncDB(time)
     socket.select(nil, nil, 600)
