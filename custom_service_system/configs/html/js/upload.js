870dbc001e54cb151ea7e243cf48511bd704cb13
zzywuwu <zzywuwu@126.com>
2015-12-31 17:26:43 +0800

    上传图片分两步提交

--- custom_service_system/configs/html/js/upload.js
+++ custom_service_system/configs/html/js/upload.js
@@ -13,19 +13,13 @@ var uploadmodule = function() {
                 ignore: "",
 
 	            rules: {
-	                record_id: {
-	                	digits: true,
-                        required: true
-	                },
+	                
                     image_name: {
                         required: true
                     }
 	            },
 	            messages:{
-                    record_id:{
-                    	digits:"请填写数字",
-                        required:"必填"
-                    },
+                   
                     image_name:{
                         required:"必填"
                     }                                            
@@ -59,10 +53,9 @@ var uploadmodule = function() {
                     }
                     else {
                         $('.portlet-body .submit_start').hide();
-                        $('.portlet-body .submit_end').show();
-                        $('.portlet-body .image_start').show();
-                        $('.portlet-body .image_end').hide();
+                        $('.portlet-body .submit_end').show();                     
                         $('#org_name').val($('#filename_desc').text());
+                        $('#record_id').val($('#record_id_first').val());
                         var submitData = v_form.serialize();
                         TendaAjax.uploadFile(v_form, submitData,
                         function(result){
@@ -70,12 +63,13 @@ var uploadmodule = function() {
                                 var url = "/temp/file/"  + $('#record_id').val()+ "_" +$('#image_name').val() + "." + fileext;
                                 $('#photo_href').attr("href",url);
                                 $('#photo_img').attr("src",url);
-                                $('.portlet-body .image_start').hide();
-                                $('.portlet-body .image_end').show();
+                                $('#upload_container .imgzoom').removeClass("hide");
+                                $('#upload_container .imginfo span').html('<h3 style="color:green;">上传成功</h3>');
+                                $('#upload_container .progress').addClass("hide");
                             }
                         },
                         function(result){
-                            $('#upload_container .imginfo').removeClass("hide");
+                            
                             $('#upload_container .imginfo span').html('<h3 style="color:red;">上传失败</h3>');
                             $('#upload_container .progress').addClass("hide");
                         });  
@@ -83,10 +77,82 @@ var uploadmodule = function() {
 				}
 			});
 
+            var v_kf_form = $('.kf-form');
+            v_kf_form.validate({
+
+                errorElement: 'span', //default input error message container
+                errorClass: 'help-inline', // default input error message class
+                focusInvalid: false, // do not focus the last invalid input
+                ignore: "",
+
+                rules: {
+                    record_id_first: {
+                        digits: true,
+                        required: true
+                    }
+                },
+                messages:{
+                    record_id_first:{
+                        digits:"请填写数字",
+                        required:"必填"
+                    }                                         
+                },
+
+                highlight: function (element) { // hightlight error inputs
+                    $(element)
+                        .closest('.help-inline').removeClass('ok'); // display OK icon
+                    $(element)
+                        .closest('.control-group').removeClass('success').addClass('error'); // set error class to the control group
+                },
+
+                unhighlight: function (element) { // revert the change dony by hightlight
+                    $(element)
+                        .closest('.control-group').removeClass('error'); // set error class to the control group
+                },
+
+                success: function (label) {
+                    label
+                        .addClass('valid').addClass('help-inline ok') // mark the current input as valid and display OK icon
+                    .closest('.control-group').removeClass('error').addClass('success'); // set success class to the control group
+                },
+
+                submitHandler: function(form){
+                    
+                    var submitData = {}; 
+                    submitData.script = "record_query_by_id";
+                    submitData.id = $("#record_id_first").val();
+                    TendaAjax.getData(submitData, function(result){
+                        if(result.error == "成功") {
+                            var obj = jQuery.parseJSON(result.user_info.image);
+                            var image_str = "";
+                            if (Array.isArray(obj)) {
+                                if (obj.length != 0) {
+                                    for (var i = 0; i < obj.length; i++) {
+                                        var path = obj[i];
+                                        var arr = obj[i].split('_',2);
+                                        var file = arr[1].split('.',2);
+                                        image_str += file[0] + "&nbsp&nbsp";
+                                    }   
+                                }
+                            }
+                            var html = '<h4 style="color:purple;">姓名： ' + result.user_info.customer_name +' (Id:'+result.user_info.customer_id+')</br>日期： '+ result.user_info.visit_date +'('+ result.user_info.visit_time +')'+'</br>事件：'+ result.user_info.visit_type + '</br>已有图片： '+ image_str +'</h4>';
+                            $('#imginfo span').html(html);
+                            $('.portlet-body .submit_first').hide();
+                            $('.portlet-body .submit_start').show();
+                        }               
+                    });
+                }
+            });
+
             $('#org_name').hide();
+            $('#record_id').hide();
+            $('.portlet-body .submit_start').hide();
             $('.portlet-body .submit_end').hide();
 
             $('#again_submit').on('click',function(){
+                window.location.reload();
+            });
+            $('#return_query').on('click',function(){
                 window.location.reload();
             });
 		}
---
+++ custom_service_system/configs/lua_scripts/record_query_by_id.lua
@@ -0,0 +1,47 @@
+local function MysqlCallback(res)
+	local _affected_rows = res.affected_rows
+	if _affected_rows == 0 then
+		return nil, WEBERR.USER_NO_EXIST
+	end
+
+	local _jsontbl = {
+		web = {
+			error = WEBERR.NO_ERR,
+			user_info = res[1]
+		}
+	}
+	return _jsontbl
+end
+
+local function ParamCheck(post)
+	if not post.web then
+		return false, WEBERR.PARAM_ERR
+	end
+
+	if not post.web.id then
+		return false, WEBERR.PARAM_ERR
+	end
+
+	if not (post.web.id ~= '') then
+		return false, WEBERR.PARAM_ERR
+	end
+
+	return true
+end
+
+local function Execute(post)
+	local _id = post.web.id
+	local _query_sql = "select *,customer.name as customer_name from record,customer where record.id = "..ngx.quote_sql_str(_id).." and record.customer_id = customer.id limit 1"
+
+	DEBUG("record_query_by_id: " .. _query_sql)
+	return mysql.query(cloud_database, _query_sql, MysqlCallback)
+end
+
+local _M = {
+	ParamCheck = ParamCheck,
+	Execute = Execute,
+	cache = {}
+}
+
+return _M
+
