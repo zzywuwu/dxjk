5e4cafb8ba37e0d58de7e6524f3ce1a601b5035b
zzywuwu <zzywuwu@126.com>
2016-01-30 00:44:21 +0800

    短信界面添加事件修改和记录跳转

--- custom_service_system/configs/html/js/sms_notify.js
+++ custom_service_system/configs/html/js/sms_notify.js
@@ -1,6 +1,8 @@
 
 var sms_notify_module = function() {
 
+	var operation_customer_id;
+	var operation_customer_name;
 	var search_str = "";
 
 	var initTableList =  function() {
@@ -56,7 +58,21 @@ var sms_notify_module = function() {
 				//sProcessing : "<img src=... /loading.gif/>"
 			},
 			
-			"aoColumnDefs": [	
+			"aoColumnDefs": [
+							{
+								"aTargets":[0],
+								"data":"id",
+								"mRender":function(data, type, full){								
+						  			return '<a href="#" class="event_action">'+data+'</a>';	
+								}
+							},	
+							{
+								"aTargets":[1],
+								"data":"customer_name",
+								"mRender": function(data, type, full) {
+									return '<a href="#" class="record" data="' + full.customer_id + '">'+data+'</a>';
+								}
+							},	
 							{
 								"aTargets":[4],
 								"mRender":function(data, type, full){
@@ -113,6 +129,58 @@ var sms_notify_module = function() {
 		jQuery("#kf_list_wrapper .dataTables_filter input").addClass("m-wrap small");
 		jQuery("#kf_list_wrapper .dataTables_length select").addClass("m-wrap small");
 
+		App.initUniform("#kf_list .checkboxes");
+
+		jQuery(".event_action").click(function(){
+
+			var arr = [];           
+            var oTable = $("#kf_list").dataTable();
+    		var nTr = $(this).parents("tr");
+    		var tmpObj = oTable.fnGetData(nTr[0]);   		
+    		arr.push(tmpObj);
+
+    		operation_customer_id = arr[0].customer_id;
+			operation_customer_name = arr[0].customer_name;
+			
+			$("#event_id").val(arr[0].id).prop("disabled", true);
+			$("#event_visit_date").val(arr[0].visit_date);
+			jQuery('#event_visit_time option').each(function(){
+				if (arr[0].visit_time == $(this).text()){
+					$(this).attr("selected",true);
+				}
+			});
+			
+			var obj = jQuery.parseJSON(arr[0].visit_type);
+			if (Array.isArray(obj)) {
+				for (var i = 0; i < obj.length; i++) {
+					jQuery('#event_visit_type option').each(function(){
+						if (obj[i] == $(this).text()){
+							$(this).attr("selected",true);
+							return false;
+						}
+					});
+				};
+			}
+
+			jQuery('#event_order_success option').each(function(){
+				if ("已预约" == $(this).text() && arr[0].order_success){
+					$(this).attr("selected",true);
+					return false;
+				}
+			});
+			$("#event_visit_doctor_name").val(arr[0].visit_doctor_name);
+			$("#event_visit_address").val(arr[0].visit_address);	
+			$("#event_remarks").val(arr[0].remarks);
+        	$("#event_modal").modal("show");	
+		});
+
+		jQuery(".record").click(function(){
+			var data = {"page":"record.html","customer_id":$(this).attr("data")};
+			TendaAjax.getHtml(data, function(result){
+				$(".page-content .container-fluid").html(result);
+			});  
+		});	
+
 		jQuery('.tooltip_view').mouseover(function(){		
 			var arr = [];           
             var oTable = $("#kf_list").dataTable();
@@ -144,8 +212,6 @@ var sms_notify_module = function() {
 			$(this).tooltip('show');
 		});
 
-		App.initUniform("#kf_list .checkboxes");
-
 		jQuery(".sms_send").click(function(){
 			$("#sms_modal_title").html('发送短信'); 
         	$("#sms_info").val($(this).attr("sms_send_info"));    
@@ -170,6 +236,88 @@ var sms_notify_module = function() {
 
 	return {
 		init: function () {
+
+			var v_event_form = $('.event-form');        
+			v_event_form.validate({
+	   			errorElement: 'span', //default input error message container
+                errorClass: 'help-inline', // default input error message class
+                focusInvalid: false, // do not focus the last invalid input
+                ignore: "",
+
+	            rules: {
+	    
+	                event_visit_date: {
+	                    required: true,
+                        date: true
+	                },
+	                event_visit_type: {
+	                    required: true,                
+	                }
+	            },
+
+	            messages:{
+            
+                    event_visit_date:{
+                        required:"必填"
+                    },
+                    event_visit_type:{
+                        required:"必填"
+                    }                                                       
+                },
+
+                highlight: function (element) { // hightlight error inputs
+                    $(element)
+                        .closest('.help-inline').removeClass('ok'); // display OK icon
+                    $(element)
+                        .closest('.control-group').removeClass('success').addClass('error'); // set error class to the control group
+                },
+
+                unhighlight: function (element) { // revert the change dony by hightlight
+                    $(element)
+                        .closest('.control-group').removeClass('error'); // set error class to the control group
+                },
+
+                success: function (label) {
+                    label
+                        .addClass('valid').addClass('help-inline ok') // mark the current input as valid and display OK icon
+                    .closest('.control-group').removeClass('error').addClass('success'); // set success class to the control group
+                },
+
+				submitHandler: function(form){
+					//根据获取的ID来进行判断，是修改还是添加
+					//TODO验证还需要进行权限是否为空的验证
+					var submitData = {};
+					submitData.script = "event_modify";
+					submitData.id = $("#event_id").val();
+					submitData.customer_id = operation_customer_id;
+					submitData.customer_name = operation_customer_name;
+					submitData.visit_date = form.event_visit_date.value;
+					submitData.visit_time = jQuery('#event_visit_time').val();					
+					if (jQuery('#event_order_success').val() == "已预约")
+						submitData.order_success = 1;
+					else
+						submitData.order_success = 0;
+					submitData.visit_address = form.event_visit_address.value;
+					submitData.visit_doctor_name = form.event_visit_doctor_name.value;
+					submitData.remarks = form.event_remarks.value;
+					
+					var visit_type_arr = [];
+					$("#event_visit_type option:selected").each(function(){			      
+			            visit_type_arr.push($(this).text());
+			        });
+			        submitData.visit_type = JSON.stringify(visit_type_arr);			       
+					TendaAjax.getData(submitData, function(result){
+						if(result.error == GLOBAL.SUCCESS) {
+							initTableList();
+							$("#event_modal").modal("hide");
+						}				
+                		else
+                			mainindex.modalwarn(result.error);;
+					});
+
+				}
+			});
+
 
 			$("#sms_confirm_button").on("click",function(){
 				var submitData = {};
