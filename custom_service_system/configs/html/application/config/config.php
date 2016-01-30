92bea3e24bc51e5fd50b1652e50ac02bcb7cc172
zzywuwu <zzywuwu@126.com>
2016-01-30 14:40:13 +0800

    修改图片大小和php日志位置

--- custom_service_system/configs/html/application/config/config.php
+++ custom_service_system/configs/html/application/config/config.php
@@ -222,7 +222,7 @@ $config['log_threshold'] = 1;
 | application/logs/ directory. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = '/var/logs/';
+$config['log_path'] = '/var/logs/php/';
 
 /*
 |--------------------------------------------------------------------------
